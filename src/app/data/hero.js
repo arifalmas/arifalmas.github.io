790b9e1e82e4bb3246b931327b5ba7362fb597eb
coderarife <freelancerarifar@gmail.com>
2023-03-02 09:40:30 +0600

    changes

--- src/app/data/hero.js
+++ src/app/data/hero.js
@@ -3,7 +3,7 @@ export const HERO = {
     subtitle: "I'm",
     name: "Arif Almas",
     typeWriter: [ "Web Developer", "Web Designer"],
-    bgImage: "/Asset/hero_bg.jpg",
+    bgImage:"/Asset/hero_bg.jpg",
     button: {
         text: "My Resume",
         url: "https://resume.showwcase.com/arifalmas.pdf"
